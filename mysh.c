e744d7cbd11a1f8e5665f838d2d17cc32d901fa6
iskwipi <neyrocaraig@gmail.com>
2026-02-02 10:08:31 +0800

    feat: added parse_line function to convert input line to a Command struct in mysh.c

--- mysh.c
+++ mysh.c
@@ -1,23 +1,65 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
+#include "mysh.h"
+
+Command parse_line(char *line) {
+    // clean line and clear struct
+    line[strcspn(line, "\n")] = 0;
+    Command cmd = {0};
+    
+    // tokenize command
+    char *token = strtok(line, " ");
+    if (token == NULL) return cmd;
+
+    // get command name
+    cmd.command = token;
+    cmd.args[0] = token;
+
+    // get command arguments
+    int i = 1;
+    token = strtok(NULL, " ");
+    while (token != NULL && i < 255) {
+        cmd.args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+
+    // ensure args is NULL-terminated
+    cmd.args[i] = NULL;
+
+    // todo:
+    // - interpret command symbols
+    // - fill in missing fields
+
+    return cmd;
+}
 
 int main() {
+    // initialize stuff
     char buffer[256];
     char *prompt = "mysh> ";
+    printf("%s", prompt);
 
-    while (true) {
-        printf("%s", prompt);
-        fgets(buffer, 256, stdin);
+    // input loop
+    while (fgets(buffer, sizeof(buffer), stdin)) {
+        Command cmd = parse_line(buffer);
 
-        if (strcmp(buffer, "exit\n") == 0){
+        // handle command
+        if (
+            cmd.command == NULL ||
+            strcmp(cmd.command, "exit") == 0
+        ) {
             break;
         }
 
+        // debug print
+        printf("Command: %s\nArgs:\n", cmd.command);
+        for (int i = 1; cmd.args[i] != NULL; i++) {
+            printf("  %d) %s\n", i, cmd.args[i]);
+        }
 
-        printf("%s", buffer);
+        // next prompt
+        printf("%s", prompt);
     }
 
     return 0;
 }
-
