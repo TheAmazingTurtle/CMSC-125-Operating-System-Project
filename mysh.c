e756d1d714bb6f840b8484e82fb7fb4484799e1d
iskwipi <neyrocaraig@gmail.com>
2026-02-02 11:21:46 +0800

    feat: added handle_command function to execute built-in commands (pwd, cd, exit) and sample echo command

--- mysh.c
+++ mysh.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include "mysh.h"
 
 Command parse_line(char *line) {
@@ -33,6 +35,44 @@ Command parse_line(char *line) {
     return cmd;
 }
 
+bool handle_command(Command *cmd) {
+    if (strcmp(cmd->command, "echo") == 0) {
+        // sample echo implementation
+        for (int i = 1; cmd->args[i] != NULL; i++) {
+            printf("%s ", cmd->args[i]);
+        }
+        printf("\n");
+    } else if (strcmp(cmd->command, "pwd") == 0) {
+        // uses getcwd() from unistd.h
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s\n", cwd);
+        } else {
+            perror("getcwd() error");
+        }
+    } else if (strcmp(cmd->command, "cd") == 0) {
+        // uses chdir() from unistd.h
+        if (cmd->args[1] == NULL) {
+            //  no argument provided
+            printf("mysh: missing operand for command: cd\n");
+        } else {
+            if (chdir(cmd->args[1]) == 0) {
+                printf("Changing directory...\n");
+            } else {
+                perror("mysh");
+            }
+        }
+    } else if (strcmp(cmd->command, "exit") == 0) {
+        // terminates shell
+        printf("Exiting mysh...\n");
+        return false;
+    } else {
+        // invalid command
+        printf("mysh: command not found: %s\n", cmd->command);
+    }
+    return true;
+}
+
 int main() {
     // initialize stuff
     char buffer[256];
@@ -43,19 +83,18 @@ int main() {
     while (fgets(buffer, sizeof(buffer), stdin)) {
         Command cmd = parse_line(buffer);
 
-        // handle command
-        if (
-            cmd.command == NULL ||
-            strcmp(cmd.command, "exit") == 0
-        ) {
-            break;
-        }
+        // handle invalid input
+        if (cmd.command == NULL) break;
 
         // debug print
-        printf("Command: %s\nArgs:\n", cmd.command);
+        printf("Command: %s\nArgs:\n%s", cmd.command, (cmd.args[1] ? "" : "(none)\n"));
         for (int i = 1; cmd.args[i] != NULL; i++) {
-            printf("  %d) %s\n", i, cmd.args[i]);
+            printf("%d) %s\n", i, cmd.args[i]);
         }
+        printf("Output:\n");
+
+        // handle command and check running status
+        if (!handle_command(&cmd)) break;
 
         // next prompt
         printf("%s", prompt);
